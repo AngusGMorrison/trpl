482a59a6ca3fd49d6a46b54622dd8d2edac75a3c
Angus Morrison <angus.morrison.ext@qonto.com>
2021-07-01 17:29:17 +0100

    Generics

---
+++ 10_generics_traits_lifetimes/generics/src/main.rs
@@ -0,0 +1,64 @@
+fn largest<T>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+struct Point<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Point<T> {
+    fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+impl Point<f32> {
+    fn distance_from_origin(&self) -> f32 {
+        (self.x.powi(2) + self.y.powi(2)).sqrt()
+    }
+}
+
+struct MixedPoint<T, U> {
+    x: T,
+    y: U,
+}
+
+impl<T, U> MixedPoint<T, U> {
+    fn mixup<V, W>(self, other: MixedPoint<V, W>) -> MixedPoint<T, W> {
+        MixedPoint {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
+    println!("integer.x = {}", integer.x());
+
+    let mp1 = MixedPoint { x: 5, y: 0.4 };
+    let mp2 = MixedPoint { x: "Hello", y: 'c' };
+    let mp3 = mp1.mixup(mp2);
+    println!("mp3.x = {}, mp3.y = {}", mp3.x, mp3.y);
+}
+
